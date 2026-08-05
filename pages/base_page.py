d29a9027934cdb662e07433877e06948c95301ac
Yurii <yurii@MacBook-Air-Veronika.local>
2026-08-05 22:23:30 +0300

    feat(core): add global toast message locators and handlers

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,8 @@
+from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 
 from pages.base import Base
+from pages.types import Locator
 
 
 class BasePage(Base):
@@ -9,3 +11,20 @@ class BasePage(Base):
     def __init__(self, driver: WebDriver):
         """Initialize the base page with a WebDriver."""
         super().__init__(driver)
+
+        self._success_message: Locator = (
+            By.CSS_SELECTOR,
+            "div.ant-message-success span:not(.anticon)",
+        )
+        self._error_message: Locator = (
+            By.CSS_SELECTOR,
+            "div.ant-message-error span:not(.anticon)",
+        )
+
+    def get_success_message_text(self) -> str:
+        """Get the text of the global success toast message."""
+        return self._get_text(self._success_message)
+
+    def get_error_message_text(self) -> str:
+        """Get the text of the global error toast message."""
+        return self._get_text(self._error_message)
