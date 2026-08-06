5d71558327995e184a3589a6ee0a36043ab8607f
movchanets <slavikmovchanetc@gmail.com>
2026-08-06 14:36:57 +0300

    Revert BaseModal to use WebDriver

--- pages/modals/base_modal.py
+++ pages/modals/base_modal.py
@@ -1,11 +1,11 @@
-from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.remote.webdriver import WebDriver
 
 from pages.base import Base
 
 
 class BaseModal(Base):
-    """Base class for all modals, representing an encapsulated UI component (COM)."""
+    """Base class for all modals."""
 
-    def __init__(self, root: WebElement):
-        """Initialize the base modal with its root WebElement."""
-        super().__init__(root)
+    def __init__(self, driver: WebDriver):
+        """Initialize the base modal with a WebDriver."""
+        super().__init__(driver)
