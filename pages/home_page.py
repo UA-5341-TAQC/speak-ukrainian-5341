1197d66051580bd0685bdde237fde53fe8dc0eda
MF-s <mfin78091@gmail.com>
2026-08-06 11:38:21 +0300

    add components

--- pages/home_page.py
+++ pages/home_page.py
@@ -3,11 +3,13 @@
 import allure
 from selenium.webdriver.common.by import By
 
-from pages.base import Base
+from pages.base_page import BasePage
+from pages.components.carousel import Carousel
+from pages.components.home_content_card import HomeContentCard
 from pages.types import Locator
 
 
-class HomePage(Base):
+class HomePage(BasePage):
     """Page object representing the Speak Ukrainian home page."""
 
     ALL_CLUBS_BUTTON: Locator = (
@@ -35,32 +37,55 @@ class HomePage(Base):
         'a[href="https://www.facebook.com/events/2754499954695563"] img.banner-image',
     )
 
+    CAROUSEL: Locator = (
+        By.CSS_SELECTOR,
+        ".about-carousel-block",
+    )
+
+    CONTENT_CARDS: Locator = (
+        By.CSS_SELECTOR,
+        ".primitive-card",
+    )
+
     @allure.step("Click 'Всі гуртки' button")
     def click_all_clubs_button(self) -> None:
         """Click the 'Всі гуртки' button."""
-        self._click_clickable(self.ALL_CLUBS_BUTTON).click()
+        self._wait_clickable(self.ALL_CLUBS_BUTTON).click()
 
     @allure.step("Switch club categories to previous")
     def click_categories_prev_arrow(self) -> None:
         """Click the previous arrow of the categories carousel."""
-        self._click_clickable(self.CATEGORIES_PREV_ARROW).click()
+        self._wait_clickable(self.CATEGORIES_PREV_ARROW).click()
 
     @allure.step("Switch club categories to next")
     def click_categories_next_arrow(self) -> None:
         """Click the next arrow of the categories carousel."""
-        self._click_clickable(self.CATEGORIES_NEXT_ARROW).click()
+        self._wait_clickable(self.CATEGORIES_NEXT_ARROW).click()
 
     @allure.step("Click 'Дізнатись більше' button in the challenge block")
     def click_challenge_learn_more_button(self) -> None:
         """Click the 'Дізнатись більше' button of the challenge block."""
-        self._click_clickable(self.CHALLENGE_LEARN_MORE_BUTTON).click()
+        self._wait_clickable(self.CHALLENGE_LEARN_MORE_BUTTON).click()
 
     @allure.step("Click 'Розмовляй' speaking club link")
     def click_speaking_club_link(self) -> None:
         """Click the 'Розмовляй' speaking club link."""
-        self._click_clickable(self.SPEAKING_CLUB_LINK).click()
+        self._wait_clickable(self.SPEAKING_CLUB_LINK).click()
 
     @allure.step("Click initiative banner image")
     def click_banner_image(self) -> None:
         """Click the initiative banner image."""
-        self._click_clickable(self.BANNER_IMAGE).click()
+        self._wait_clickable(self.BANNER_IMAGE).click()
+
+    @allure.step("Get carousel")
+    def get_carousel(self) -> Carousel:
+        """Return the carousel component."""
+        return Carousel(self._find_element(self.CAROUSEL))
+
+    @allure.step("Get content cards")
+    def get_content_cards(self) -> list[HomeContentCard]:
+        """Return all home page content cards."""
+        return [
+            HomeContentCard(card)
+            for card in self.driver.find_elements(*self.CONTENT_CARDS)
+        ]
