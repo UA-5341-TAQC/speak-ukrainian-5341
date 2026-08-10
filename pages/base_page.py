75d3da5868e82a05567e792a24405a4b61c6ee1e
Yurii Koliada <wonters1223@gmail.com>
2026-08-10 13:16:38 +0300

    refactor: extract locators from __init__ to class level in BasePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -8,23 +8,23 @@ from pages.types import Locator
 class BasePage(Base):
     """Base class for all pages."""
 
+    success_message: Locator = (
+        By.CSS_SELECTOR,
+        "div.ant-message-success span:not(.anticon)",
+    )
+    error_message: Locator = (
+        By.CSS_SELECTOR,
+        "div.ant-message-error span:not(.anticon)",
+    )
+
     def __init__(self, driver: WebDriver):
         """Initialize the base page with a WebDriver."""
         super().__init__(driver)
 
-        self._success_message: Locator = (
-            By.CSS_SELECTOR,
-            "div.ant-message-success span:not(.anticon)",
-        )
-        self._error_message: Locator = (
-            By.CSS_SELECTOR,
-            "div.ant-message-error span:not(.anticon)",
-        )
-
     def get_success_message_text(self) -> str:
         """Get the text of the global success toast message."""
-        return self._get_text(self._success_message)
+        return self._get_text(self.success_message)
 
     def get_error_message_text(self) -> str:
         """Get the text of the global error toast message."""
-        return self._get_text(self._error_message)
+        return self._get_text(self.error_message)
