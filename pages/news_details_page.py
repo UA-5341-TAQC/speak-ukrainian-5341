91b6f02b4fa4a9890792f31efadf78f296e188f7
MF-s <mfin78091@gmail.com>
2026-08-13 18:40:48 +0300

    fix 2

--- pages/news_details_page.py
+++ pages/news_details_page.py
@@ -2,7 +2,6 @@
 
 import allure
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as ec
 
 from pages.base_page import BasePage
