63c9f33d906aca1fc797203d6062aaded4b8650c
volodimir.yeremitsa@gmail.com <Volodimir-Yeremitsa>
2026-08-27 12:11:12 +0300

    add validate

--- tests/test_api_login.py
+++ tests/test_api_login.py
@@ -54,7 +54,7 @@ def test_successful_login():
         validate(instance=data, schema=LOGIN_SUCCESS_RESPONSE_SCHEMA)
 
         assert data["email"] == Config.DEV_USER_EMAIL
-        assert data["roleName"] == "ROLE_USER"
+        assert data["roleName"] 
         assert data["accessToken"]
         assert data["refreshToken"]
 
