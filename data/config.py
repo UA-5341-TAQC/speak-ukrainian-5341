a195bfc3063b0222f5ecc076921b07457f6d5d8a
volodimir.yeremitsa@gmail.com <Volodimir-Yeremitsa>
2026-09-03 12:24:22 +0300

    New API_USER

--- data/config.py
+++ data/config.py
@@ -33,8 +33,5 @@ class Config:
     MANAGER_EMAIL: str = os.getenv("MANAGER_EMAIL", "")
     MANAGER_PASSWORD: str = os.getenv("MANAGER_PASSWORD", "")
 
-    DEV_API_URL: str = os.getenv("DEV_API_URL", "")
-    DEV_USER_EMAIL: str = os.getenv("DEV_USER_EMAIL", "")
-    DEV_USER_PASSWORD: str = os.getenv("DEV_USER_PASSWORD", "")
     ADMIN_EMAIL: str = os.getenv("ADMIN_EMAIL", "")
     ADMIN_PASSWORD: str = os.getenv("ADMIN_PASSWORD", "")
